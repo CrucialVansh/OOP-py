6bd81598654b20b4ec837a93158e6f87e302289a
Vansh <z5482641@ad.unsw.edu.au>
2024-02-23 23:57:45 +1100

    new class

---
+++ Car/car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, colour, odometer):
+        self.colour = colour
+        self.odometer = odometer
+
+    def __str__(self):
+        return f"The {self.colour} car has {self.odometer} kilometers"
